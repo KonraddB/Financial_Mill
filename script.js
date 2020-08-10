a6a4b93bdb8e3150b4a96c5056e20b3782522e8f
Konrad Bieniek <konrad.bieniek@ec2.pl>
2020-08-10 21:47:20 +0200

    Initial commit

--- script.js
+++ script.js
@@ -11,6 +11,45 @@ header.addEventListener('click', function() {
       }
 })
 
+// header fixed
+
+window.onscroll=function(){
+  const docScrollTop=document.documentElement.scrollTop;
+
+  if(window.innerWidth>991){
+    if(docScrollTop>100){
+      document.querySelector("header").classList.add("fixed")
+    }
+    else{
+    document.querySelector("header").classList.remove("fixed")	
+    }
+  }
+}    
+
+
+// navbar links
+
+const navbar = document.querySelector(".navbar");
+    a=navbar.querySelectorAll("a");
+    
+    a.forEach(function(element){
+       element.addEventListener("click",function(){
+          for(let i=0; i<a.length; i++){
+            a[i].classList.remove("active");
+          }
+         this.classList.add("active")
+         document.querySelector(".navbar").classList.toggle("show");
+       })
+    })
+
+// ham-burger
+
+const hamBurger=document.querySelector(".ham-burger");
+
+hamBurger.addEventListener("click",function(){
+     document.querySelector(".navbar").classList.toggle("show");
+})
+
 // slider home
 
 const slides = document.querySelector('.slider').children;
@@ -98,3 +137,32 @@ let index = 0;
 
   let timer=setInterval(autoPlay,6000);
 
+// SIDEBAR
+
+  $(document).ready(function() {
+
+    function toggleSidebar() {
+      $(".openSidebar").toggleClass("active");
+      $("main").toggleClass("move-to-left");
+      $(".sidebar-item").toggleClass("active");
+    }
+  
+    $(".button").on("click tap", function() {
+      toggleSidebar();
+    });
+  
+    $(document).keyup(function(e) {
+      if (e.keyCode === 27) {
+        toggleSidebar();
+      }
+    });
+  
+  });
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
