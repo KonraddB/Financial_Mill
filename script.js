9ee211c0c78f0ff5c53f84d875ad223987035224
Konrad Bieniek <konrad.bieniek@ec2.pl>
2020-08-31 21:39:39 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+      currentSlide: 0,
+      isPreviousSlide: false,
+      isFirstLoad: true,
+      slides: [
+          {
+              headlineFirstLine: "Otwarte Konto Oszczędnościowe",
+              headlineSecondLine: "Jedno oko na maroko, drugie w banku",
+              sublineFirstLine: "Nihil sub sole",
+              sublineSecondLine: "novum",
+              bgImg: "images/eye-3440464_1920.jpg",
+              rectImg: "images/eye-3440464_1920.jpg"
+          },
+          {
+              headlineFirstLine: "Rachunek Oszczędnościwo-Rozliczeniowy",
+              headlineSecondLine: "Pierwszym krokiem do bogactwa...",
+              // sublineFirstLine: "Il n'y a rien de neuf sous",
+              // sublineSecondLine: "le soleil",
+              bgImg: "images/piggy-bank-2889046_1920.jpg",
+              rectImg: "images/piggy-bank-2889046_1920.jpg"
+          },
+          {
+              headlineFirstLine: "Lokata",
+              headlineSecondLine: "Zamrażarka z kostkami lodu",
+              // sublineFirstLine: "Τίποτα καινούργιο κάτω από",
+              // sublineSecondLine: "τον ήλιο",
+              bgImg: "images/ice-cubes-3506781_1920.jpg",
+              rectImg: "images/ice-cubes-3506781_1920.jpg"
+          }
+      ]
+  },
+mounted: function () {
+  var productRotatorSlide = document.getElementById("app");
+      var startX = 0;
+      var endX = 0;
+
+      productRotatorSlide.addEventListener("touchstart", (event) => startX = event.touches[0].pageX);
+
+      productRotatorSlide.addEventListener("touchmove", (event) => endX = event.touches[0].pageX);
+
+      productRotatorSlide.addEventListener("touchend", function(event) {
+          var threshold = startX - endX;
+
+          if (threshold < 150 && 0 < this.currentSlide) {
+              this.currentSlide--;
+          }
+          if (threshold > -150 && this.currentSlide < this.slides.length - 1) {
+              this.currentSlide++;
+          }
+      }.bind(this));
+},
+  methods: {
+      updateSlide(index) {
+          index < this.currentSlide ? this.isPreviousSlide = true : this.isPreviousSlide = false;
+          this.currentSlide = index;
+          this.isFirstLoad = false;
+      }
+  }
+})
+
+
 // navbar
 
 const header = document.querySelector('header');
@@ -172,3 +235,4 @@ let index = 0;
     document.getElementById("mySidenav").style.width = "0";
   }
 
+  
